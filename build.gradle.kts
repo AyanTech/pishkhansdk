6968faad3c20b3de2e3638e02fd361aad8d5eaff
Zahra Hosseini <s.z.hosseini9473@gmail.com>
2024-02-28 13:12:35 +0330

    Update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,8 +11,8 @@ buildscript {
         gradlePluginPortal()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.2.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+        classpath("com.android.tools.build:gradle:8.2.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
     }
 }
 
