c11f3378e93cec0bcae057fa0fe69e993951cb28
Zahra Hosseini <s.z.hosseini9473@gmail.com>
2024-03-17 11:32:59 +0330

    update gradle

--- pishkhansdk/src/main/java/ir/ayantech/pishkhansdk/ui/bottom_sheet/OtpBottomSheetDialog.kt
+++ pishkhansdk/src/main/java/ir/ayantech/pishkhansdk/ui/bottom_sheet/OtpBottomSheetDialog.kt
@@ -96,6 +96,7 @@ class OtpBottomSheetDialog(
                     }
 
                     else -> {
+                        dismiss()
                         callback(enterOtpCodeLayout.getText())
                     }
                 }
