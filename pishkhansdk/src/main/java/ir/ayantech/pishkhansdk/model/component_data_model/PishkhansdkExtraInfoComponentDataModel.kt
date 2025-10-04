9b7d07529aff7059b9a93add0ed4062522e2b299
S.Shahsavari <samanshahsavari3@gmail.com>
2025-10-04 17:20:19 +0330

    add extra info component data model in models path of project

---
+++ pishkhansdk/src/main/java/ir/ayantech/pishkhansdk/model/component_data_model/PishkhansdkExtraInfoComponentDataModel.kt
@@ -0,0 +1,7 @@
+package ir.ayantech.pishkhansdk.model.component_data_model
+
+import ir.ayantech.pishkhansdk.model.app_logic.ExtraInfo
+
+data class PishkhansdkExtraInfoComponentDataModel(
+    val extraInfoItems: List<ExtraInfo>,
+)
--- pishkhansdk/src/main/java/ir/ayantech/pishkhansdk/ui/components/PishkhansdkExtraInfoComponent.kt
+++ pishkhansdk/src/main/java/ir/ayantech/pishkhansdk/ui/components/PishkhansdkExtraInfoComponent.kt
@@ -1,20 +1,16 @@
 package ir.ayantech.pishkhansdk.ui.components
 
-import androidx.core.view.forEach
 import ir.ayantech.pishkhansdk.R
 import ir.ayantech.pishkhansdk.databinding.PishkhansdkComponentExtraInfoBinding
 import ir.ayantech.pishkhansdk.helper.extensions.setEndDrawable
 import ir.ayantech.pishkhansdk.model.app_logic.ExtraInfo
+import ir.ayantech.pishkhansdk.model.component_data_model.PishkhansdkExtraInfoComponentDataModel
 import ir.ayantech.pishkhansdk.ui.adapter.PishkhansdkExtraInfoComponentItemsAdapter
 import ir.ayantech.whygoogle.helper.changeVisibility
 import ir.ayantech.whygoogle.helper.delayedTransition
 import ir.ayantech.whygoogle.helper.isVisible
 import ir.ayantech.whygoogle.helper.safeGet
 import ir.ayantech.whygoogle.helper.verticalSetup
-
-data class PishkhansdkExtraInfoComponentDataModel(
-    val extraInfoItems: List<ExtraInfo>,
-)
 
 fun PishkhansdkComponentExtraInfoBinding.initExtraInfoComponent(data: PishkhansdkExtraInfoComponentDataModel) {
 
