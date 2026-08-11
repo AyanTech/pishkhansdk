e3c74b8d7b57e64db141090953fb4b55cc21eec4
Mohammad <mj.alijani@gmail.com>
2026-08-11 17:04:31 +0330

    chore(version): change publish version to 1.2.10-alpha04

--- pishkhansdk/build.gradle.kts
+++ pishkhansdk/build.gradle.kts
@@ -64,7 +64,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.AyanTech"
             artifactId = "pishkhansdk"
-            version = "2.0.0"
+            version = "1.2.10-alpha04"
 
             afterEvaluate {
                 from(components["release"])
